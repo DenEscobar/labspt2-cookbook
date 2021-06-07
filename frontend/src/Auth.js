05bd43a95185b9098b81bf16dd6273e0076e1a98
Denise Escobar <descobar1490@gmail.com>
2021-06-07 08:55:35 -0500

    trying to get authorization working again

--- frontend/src/Auth.js
+++ frontend/src/Auth.js
@@ -3,7 +3,7 @@ import auth0 from 'auth0-js';
 export default class Auth{
     auth0 =new auth0.WebAuth({
         domain: 'kookr-app.auth0.com',
-        clientID: 'bDx67QkY7LDZi4lATMZn8eSySUOvr8Kk',
+        clientID: '1076142196734-jcjqv8o2ubvhmsq36l64maiqecvclg73.apps.googleusercontent.com',
         redirectUri: 'https://kookr.netlify.com/create',
         // audience: '' ??
         //responseType:  ''
